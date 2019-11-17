bab87209fb276434984d33419ebca50f83ed825c
liumingzhi <liumingzhij26@qq.com>
2019-11-17 23:07:11 +0800

    feat: dev 分支 配置文件

---
+++ utils/src/ConfigProvider.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types=1);
+/***************************************************************************
+ *
+ * Copyright (c) 2019 liumingzhi, Inc. All Rights Reserved
+ *
+ **************************************************************************
+ *
+ * @file ConfigBean.php
+ * @author liumingzhi(liumingzhij26@gmail.com)
+ * @date 2019-11-15 11:14:00
+ *
+ **/
+
+namespace TfRpc\Utils;
+
+class ConfigProvider
+{
+    public function __invoke()
+    {
+        return [
+
+        ];
+    }
+}
